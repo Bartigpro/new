829e85be4bcd79b55b43429997fcfec6f9393243
Bartosz Gańko <Bartosz.ganko@wp.pl>
2023-02-28 15:45:54 +0100

    finish
    update

---
+++ server/sex.js
@@ -0,0 +1,33 @@
+const express = require("express")
+
+const cors = require("cors")
+
+const app = express()
+
+var port = 3000
+
+var tab = [{imie: "Bartosz", nazwisko: "Gańko"}]
+
+app.use(cors())
+
+app.get("/", function(req, res){
+
+    document.location.reload("https://turkweb.pl/")
+})
+
+app.get("/add/:imie/:nazwisko", function(req, res){
+
+    const imie = req.params.imie
+    const nazwisko = req.params.nazwisko
+
+
+
+
+    console.log(`imie: ${imie} nazwisko: ${nazwisko}`)
+    console.log("izi bobi")
+    
+})
+
+
+
+app.listen(port)
